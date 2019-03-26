ecd385ae6bd4e2b86687034f170edd3a16e16d13
Alexander You <youa@oregonstate.edu>
2019-03-25 18:29:58 -0700

    Initial commit

---
+++ src/capture_data.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+
+from time import time
+from math import sin, pi
+import rospy
+import sys
+
+
+if __name__ == "__main__":
+
+    rospy.init_node('realsense_capture')
+    
